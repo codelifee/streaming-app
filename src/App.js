3565c9da6abe83fa5502a293a1b479e8b5fce793
codelifee <suminjung09@gmail.com>
2021-02-02 19:55:22 +0900

    feat: create banner and nav component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import './App.css';
 import Row from './Row';
 import React, {useState} from 'react';
 import requests from './requests'
+import Banner from './Banner'
+import Nav from './Nav'
 
 function App() {
   const [movies, setMovies] = useState([]);
 
   return (
-    <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row 
+      title="NETFLIX ORIGINALS"
+      fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+import './Nav.css'
+
+function Nav() {
+
+    const[show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, [])
+
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+            <img 
+            className="nav__logo"
+            src={process.env.PUBLIC_URL + 'img/logo.png'}
+            alt="netflix"
+            />
+
+            <img 
+            className="nav__avatar"
+            src={process.env.PUBLIC_URL + 'img/logo.png'}
+            alt="netflix"
+            />
+        </div>
+    )
+}
+
+export default Nav
